45e67c0bdaa5eb2ec1a6bed89a2412e1fb5867d0
Kopyl10 <kopyl10@gmail.com>
2025-07-08 23:55:30 -0700

    7877

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { fetchImages } from './js/pixabay-api.js';
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
 
 const form = document.querySelector('#search-form');
@@ -8,6 +9,8 @@ const loadMoreBtn = document.querySelector('.load-more');
 
 let currentPage = 1;
 let searchQuery = '';
+
+let lightbox = new SimpleLightbox('.gallery a');
 
 form.addEventListener('submit', async e => {
   e.preventDefault();
@@ -38,21 +41,51 @@ form.addEventListener('submit', async e => {
   }
 });
 
+loadMoreBtn.addEventListener('click', async () => {
+  currentPage += 1;
+
+  try {
+    const data = await fetchImages(searchQuery, currentPage);
+    renderGallery(data.hits);
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+
+    const totalPages = Math.ceil(data.totalHits / 40);
+    if (currentPage >= totalPages) {
+      loadMoreBtn.hidden = true;
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  } catch (error) {
+    Notiflix.Notify.failure('Something went wrong while loading more images.');
+  }
+});
+
 function renderGallery(images) {
   const markup = images
     .map(
       image => `
-    <div class="photo-card">
-      <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-      <div class="info">
-        <p class="info-item"><b>Likes</b> ${image.likes}</p>
-        <p class="info-item"><b>Views</b> ${image.views}</p>
-        <p class="info-item"><b>Comments</b> ${image.comments}</p>
-        <p class="info-item"><b>Downloads</b> ${image.downloads}</p>
-      </div>
-    </div>`
+      <div class="photo-card">
+        <a class="gallery__link" href="${image.largeImageURL}">
+          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+        </a>
+        <div class="info">
+          <p class="info-item"><b>Likes</b> ${image.likes}</p>
+          <p class="info-item"><b>Views</b> ${image.views}</p>
+          <p class="info-item"><b>Comments</b> ${image.comments}</p>
+          <p class="info-item"><b>Downloads</b> ${image.downloads}</p>
+        </div>
+      </div>`
     )
     .join('');
 
   gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh(); // 🔄 update lightbox with new images
 }
